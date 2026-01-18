f12709a2251137b9dccdb8ac0e0208ecbdc8c4d6
Magnum Monkey <magnummonkey0@gmail.com>
2026-01-17 20:04:43 -0500

    Add CORS for GitHub Pages

--- server.py
+++ server.py
@@ -24,11 +24,14 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://magummonkey.github.io",
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
     ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 
